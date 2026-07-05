909f011855027796cea61317457c41dd3703be82
sarahh-h <sghyatt23@gmail.com>
2026-07-05 12:41:11 -0400

    Adding needed SpringBoot files to project

---
+++ src/main/java/com/csc340HW3/assignmnet3/Assignmnet3Application.java
@@ -0,0 +1,13 @@
+package com.csc340HW3.assignmnet3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignmnet3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignmnet3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/csc340HW3/assignmnet3/Assignmnet3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csc340HW3.assignmnet3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignmnet3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
